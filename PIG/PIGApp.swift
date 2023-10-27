ada3a110e65b053ce202ad101a6960536639ee01
Betzy V. Rosiles Moreno <brosilesmoreno@bsd220.org>
2023-10-27 09:38:20 -0500

    Initial Commit

---
+++ PIG/PIGApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIGApp.swift
+//  PIG
+//
+//  Created by Betzy Moreno on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PIGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
