6488d702ea76dbef6d94fdbd4eb974c87c1f27dd
Betzy V. Rosiles Moreno <brosilesmoreno@bsd220.org>
2023-10-30 10:03:17 -0500

    "Added CustomButtonStyle"

--- PIG/ContentView.swift
+++ PIG/ContentView.swift
@@ -23,22 +23,43 @@ struct ContentView: View {
                     .frame(width: 150, height: 150)
                     .rotationEffect(.degrees(rotation))
                     .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
-                        .padding(50)
-                                      Spacer()
-                                      CustomText(text: "Turn Score: \(turnScore)")
-                                      CustomText(text: "Game Score: \(gameScore)")
-                                      }
-                                      }
-                                      }
-                                      }
-                                      struct CustomText: View {
-                        let text: String
-                        var body: some View {
-                            Text(text).font(Font.custom("Market Felt", size: 36))
-                        }
+                    .padding(50)
+                Spacer()
+                CustomText(text: "Turn Score: \(turnScore)")
+                HStack {
+                    Button("Roll") {
+                        
                     }
-                                      struct ContentView_Previews: PreviewProvider {
-                        static var previews: some View {
-                            ContentView()
-                        }
+                    .buttonStyle(CustomButtonStyle())
+                    Button("Hold") {
+                        
                     }
+                    .buttonStyle(CustomButtonStyle())
+                }
+                CustomText(text: "Game Score: \(gameScore)")
+            }
+        }
+    }
+}
+struct CustomText: View {
+    let text: String
+    var body: some View {
+        Text(text).font(Font.custom("Marker Felt", size: 36))
+    }
+}
+struct CustomButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(width: 50)
+            .font(Font.custom("Marker Felt", size: 24))
+            .padding()
+            .background(.red).opacity(configuration.isPressed ? 0.0 : 1.0)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
